657fac7416f42560647e0cabf661f93bb1f5f0fe
PerroneEros <166446910+PerroneEros@users.noreply.github.com>
2024-08-16 16:12:40 -0300

    Delete Programacion/PYTHON directory

--- Programacion/PYTHON/Ejercicio9.py
+++
@@ -1,8 +0,0 @@
-a = int(input("ingrese un numero:"))
-b = int(input("ingrese un numero:"))
-x = int(input("ingrese un numero:"))
-
-print(f"los multiplos de {x} son:")
-for i in range(a,b+1,1):
-    if ((x % i) == 0):
-        print(i,end=", ") 
