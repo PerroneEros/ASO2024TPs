c018f919356edfd093facd008e0c554f80504e09
PerroneEros <166446910+PerroneEros@users.noreply.github.com>
2024-10-31 09:49:23 -0300

    Update datos1.sql

--- Base de Datos/TP4 EJERCICIO 18/datos1.sql
+++ Base de Datos/TP4 EJERCICIO 18/datos1.sql
@@ -4,7 +4,7 @@ INSERT INTO Proyectos (NombreProyecto, FechaInicio, FechaFin, Estado) VALUES
     ('Proyecto C', '2023-03-01', '2023-07-01', 'Retrasado'),
     ('Proyecto D', '2023-04-01', '2023-12-01', 'En Curso'),
     ('Proyecto E', '2023-05-01', NULL, 'En Curso'),
-	('Proyecto K', '2023-01-10', '2023-05-15', 'Completado'), 
+    ('Proyecto K', '2023-01-10', '2023-05-15', 'Completado'), 
     ('Proyecto L', '2023-06-01', '2023-11-01', 'En Curso'),   
     ('Proyecto M', '2023-07-01', '2023-09-30', 'En Curso'),   
     ('Proyecto N', '2023-09-01', '2023-10-15', 'Retrasado'),  
